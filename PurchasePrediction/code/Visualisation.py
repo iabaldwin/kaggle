2b360f5f3ae8f0618752d2bd5c5987c8a3e8b17d
Ian Baldwin <ian@iabaldwin.com>
2014-04-07 20:55:29 -0700

    Visualisation stub

--- PurchasePrediction/code/Visualisation.py
+++ PurchasePrediction/code/Visualisation.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 import os, json
-from collections import defaultdict
+import pprint
+import numpy
 
+from collections import defaultdict
 from Project import Directories
+
+from matplotlib import cm
+from matplotlib.colors import rgb2hex
 
 class Visualiser(object):
     pass
@@ -22,15 +27,36 @@ class StateVisualiser(Visualiser):
             self.keys[ key ] = index
       
         states = {} 
-
-        states_data = defaultdict(int)
+        states_data = {}
 
         for index, line in enumerate( input_data ):
             state = line.split(',')[ self.keys['state'] ]
-            states_data[ state ] += 1
+            
+            if state in states_data:
+                states_data[ state ]['count'] += 1
+            else:
+                states_data[ state ] = {}
+                states_data[ state ]['count'] = 0
 
-        states['fill_keys'] = [ '#FFFFFF', '#000000' ] 
+        # Compute scaling
+        scaling = numpy.max( [ value[1]['count'] for value in states_data.iteritems() ] )
+
+        # Assign colors
+        jet_colormap = cm.get_cmap('Blues')
+
+        fills = {}
+
+        for key, value in states_data.iteritems():
+            fills[ key ] = rgb2hex( jet_colormap(float(value['count'])/scaling) )
+
+        fills['defaultFill'] = rgb2hex( jet_colormap(0) )
+
+        # Assign fill keys
+        for key, value in states_data.iteritems():
+            states_data[ key ][ 'fillKey' ] = key
+       
         states['states_data'] = states_data
+        states['fill_keys'] = fills
 
         with open( os.path.join( Directories.Stage(), 'states.json' ), 'w' ) as handle:
             json.dump( states, handle )
--- PurchasePrediction/code/states.js
+++ PurchasePrediction/code/states.js
@@ -1,30 +1,24 @@
 function run()
 {
-
     d3.json("stage/states.json", function(error, json) {
 
         if (error) return console.warn(error);
         
         data = json;
 
-        console.log( data.states_data )
+        console.log( data )
 
         var bubble_map = new Datamap({
             scope: 'usa',
             element: document.getElementById("map"),
+                        
+            fills: data.fill_keys,
+            data: data.states_data,
+           
             geographyConfig: {
-                popupOnHover: false,
-                highlightOnHover: false
-            },
-            highlightBorderColor: '#000000',
-            highlightBorderWidth: 3,
-            fills: {
-                'light': '#667FAF',
-                defaultFill: '#888888'
-            },
-            
-            data: data.states_data
-                  
+                    highlightBorderColor: '#ff0000',
+                    highlightBorderWidth: 3,
+            }
         });
 
     });
